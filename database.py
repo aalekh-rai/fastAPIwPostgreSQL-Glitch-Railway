27f0df584728585ccbe4f0414807a2442e156e6d
aalekh-rai <aalekh.rai@futurense.com>
2024-12-27 15:43:17 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define your database URL (replace with actual credentials if needed)
-DATABASE_URL = "postgresql://postgres:aalekh45@localhost/my_team2"
-
-# Create the database engine
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
-
-# Create a sessionmaker bound to the engine
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base class for models
 Base = declarative_base()
